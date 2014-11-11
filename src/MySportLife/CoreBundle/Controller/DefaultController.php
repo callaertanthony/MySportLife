ca85e4616eb0572a81d80b3084bd8b157698e1d2
root <root@localhost.localdomain>
2014-11-11 15:47:29 +0100

    precedently commit files missed

--- src/MySportLife/CoreBundle/Controller/DefaultController.php
+++ src/MySportLife/CoreBundle/Controller/DefaultController.php
@@ -8,6 +8,9 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
+        if(false === $this->get('security.context')->isGranted('IS_AUTHENTICATED_REMEMBERED')){
+            return $this->redirect($this->generateUrl('msl_user_login'));
+        }
         return $this->render('MSLTwigBundle::layout.html.twig');
     }
 }
--- src/MySportLife/UserBundle/Controller/LoginController.php
+++ src/MySportLife/UserBundle/Controller/LoginController.php
@@ -2,14 +2,62 @@
 
 namespace MySportLife\UserBundle\Controller;
 
+use FOS\UserBundle\FOSUserEvents;
+use FOS\UserBundle\Event\FormEvent;
+use FOS\UserBundle\Event\GetResponseUserEvent;
+use FOS\UserBundle\Event\FilterUserResponseEvent;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use FOS\UserBundle\Model\UserInterface;
 
 class LoginController extends Controller
 {
-    public function DefaultAction()
+    public function DefaultAction(Request $request)
     {
+        /** @var $formFactory \FOS\UserBundle\Form\Factory\FactoryInterface */
+        $formFactoryRegistration = $this->get('fos_user.registration.form.factory');
+        /** @var $userManager \FOS\UserBundle\Model\UserManagerInterface */
+        $userManager = $this->get('fos_user.user_manager');
+        /** @var $dispatcher \Symfony\Component\EventDispatcher\EventDispatcherInterface */
+        $dispatcher = $this->get('event_dispatcher');
+
+        $user = $userManager->createUser();
+        $user->setEnabled(true);
+
+        $event = new GetResponseUserEvent($user, $request);
+        $dispatcher->dispatch(FOSUserEvents::REGISTRATION_INITIALIZE, $event);
+
+        if (null !== $event->getResponse()) {
+            return $event->getResponse();
+        }
+        
+        $formRegistration = $formFactoryRegistration->createForm();
+        $formRegistration->setData($user);
+
+        $formRegistration->handleRequest($request);
+
+        if ($formRegistration->isValid()) {
+            $event = new FormEvent($formRegistration, $request);
+            $dispatcher->dispatch(FOSUserEvents::REGISTRATION_SUCCESS, $event);
+
+            $userManager->updateUser($user);
+
+            if (null === $response = $event->getResponse()) {
+                $url = $this->generateUrl('fos_user_registration_confirmed');
+                $response = new RedirectResponse($url);
+            }
+
+            $dispatcher->dispatch(FOSUserEvents::REGISTRATION_COMPLETED, new FilterUserResponseEvent($user, $request, $response));
+
+            return $response;
+        }
+
         return $this->render('MSLUserBundle:Login:Default.html.twig', array(
-                // ...
-            ));    }
+                'form' => $formRegistration->createView(),
+            ));
+    }
 
 }
